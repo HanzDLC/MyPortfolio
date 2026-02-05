587a9115f4e23a773e6c4f7340be9daa91bda83d
HanzDLC <hanzyeahyuh12@gmail.com>
2026-02-05 19:18:43 +0800

    feeature first commit

--- app.py
+++ app.py
@@ -15,6 +15,11 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+
 if __name__ in "__main__":
     app.run(debug=True)
 
