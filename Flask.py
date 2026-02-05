390bcb3240534caffadd78dc11f62811888bb94c
HanzDLC <hanzyeahyuh12@gmail.com>
2026-02-05 09:29:54 +0800

    First commit for portfolio

---
+++ Flask.py
@@ -0,0 +1,21 @@
+
+# flask.py
+
+from flask import Flask, render_template
+from flask_scss import Scss
+from flask_mysqldb import MySQLdb
+
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ in "__main__":
+    app.run(debug=True)
+
+
